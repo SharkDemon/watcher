30ef6ddfafd6324d2699258954e4df7a20c8bfcc
RD <rory.c.dickinson@gmail.com>
2020-11-07 18:28:46 -0600

    update vehicle color

--- src/main/java/com/simco/watcher/model/VehicleColor.java
+++ src/main/java/com/simco/watcher/model/VehicleColor.java
@@ -4,17 +4,27 @@ public enum VehicleColor {
 
     // Colors chosen per https://www.carmax.com/articles/car-color-popularity
 
-    BLACK,
-    BLUE,
-    BROWN,
-    GOLD,
-    GRAY,
-    GREEN,
-    ORANGE,
-    RED,
-    SILVER,
-    TAN,
-    WHITE,
-    YELLOW
+    BLACK("Black"),
+    BLUE("Blue"),
+    BROWN("Brown"),
+    GOLD("Gold"),
+    GRAY("Gray"),
+    GREEN("Green"),
+    ORANGE("Orange"),
+    RED("Red"),
+    SILVER("Silver"),
+    TAN("Tan"),
+    WHITE("White"),
+    YELLOW("Yellow");
+
+    private final String displayName;
+
+    VehicleColor(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 
 }
