6b1602d2547e178f21c2eb2908e90ec321ead140
RD <rory.c.dickinson@gmail.com>
2020-11-08 02:15:40 -0600

    add makes

--- src/main/java/com/simco/watcher/model/VehicleMake.java
+++ src/main/java/com/simco/watcher/model/VehicleMake.java
@@ -2,10 +2,35 @@ package com.simco.watcher.model;
 
 public enum VehicleMake {
 
+    ACURA("Acura"),
+    AUDI("Audi"),
+    BMW("BMW"),
+    BUICK("Buick"),
+    CADILLAC("Cadillac"),
     CHEVROLET("Chevrolet"),
+    CHRYSLER("Chrysler"),
+    DODGE("Dodge"),
+    FERRARI("Ferrari"),
     FORD("Ford"),
+    GMC("GMC"),
+    HONDA("Honda"),
+    HYUNDAI("Hyundai"),
+    JAGUAR("Jaguar"),
+    JEEP("Jeep"),
+    KIA("Kia"),
+    LINCOLN("Lincoln"),
     MAZDA("Mazda"),
-    TOYOTA("Toyota");
+    MERCEDES("Mercedes"),
+    NISSAN("Nissan"),
+    PONTIAC("Pontiac"),
+    PORSCHE("Porsche"),
+    RAM("Ram"),
+    SUBARU("Subaru"),
+    SUZUKI("Suzuki"),
+    TESLA("Tesla"),
+    TOYOTA("Toyota"),
+    VOLKSWAGEN("Volkswagen"),
+    VOLVO("Volvo");
 
     private final String displayName;
 
